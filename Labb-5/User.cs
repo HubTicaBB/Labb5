7d716f59de7e804f871a3e3e463d44118fe8dcd8
Andreea Nenciu <andreeanen@yahoo.com>
2019-10-15 10:51:51 +0200

    Add User class

---
+++ Labb-5/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Labb_5
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public User(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
