8cb0acfa103de0be0d2e60fb88ae2d86fc889df0
Tijana <tijana.ilic87@protonmail.com>
2019-10-15 15:51:58 +0200

    Add changeButton click event, fix enable/disable buttons

--- Labb-5/MainWindow.xaml.cs
+++ Labb-5/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -13,25 +14,45 @@ namespace Labb_5
         public MainWindow()
         {
             InitializeComponent();
-
-
         }
 
-        private void EnableButtons()
+        private void EnableAddButton()
         {
             addUserButton.IsEnabled = (nameBox.Text != "" && emailBox.Text != "");
-            editUserButton.IsEnabled = (nameBox.Text != "" && emailBox.Text != "");
-            deleteUserButton.IsEnabled = (nameBox.Text != "" && emailBox.Text != "");
+        }
+
+        private void EnableChangeButton()
+        {
+            changeUserButton.IsEnabled = userListBox.SelectedItem != null;
+        }
+
+        private void EnableDeleteButton()
+        {
+            deleteUserButton.IsEnabled = userListBox.SelectedItem != null;
         }
 
         private void nameBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            EnableButtons();
+            if (userListBox.SelectedItem == null)
+            {
+                EnableAddButton();
+            }
+            else
+            {
+                EnableChangeButton();
+            }
         }
 
         private void emailBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            EnableButtons();
+            if (userListBox.SelectedItem == null)
+            {
+                EnableAddButton();
+            }
+            else
+            {
+                EnableChangeButton();
+            }
         }
 
         private void addUserButton_Click(object sender, RoutedEventArgs e)
@@ -42,16 +63,45 @@ namespace Labb_5
             userListBox.Items.Refresh();
             nameBox.Text = "";
             emailBox.Text = "";
-
         }
 
         private void userListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            EnableChangeButton();
+            EnableDeleteButton();
             foreach (var item in userList)
             {
                 if (item == userListBox.SelectedItem)
                 {
                     userInfoLabel.Content = $"User name\t {item.Name,30}\nE-Mail\t\t {item.Email,30}";
+                }
+            }
+        }
+
+        private void changeUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            deleteUserButton.IsEnabled = false;
+            nameBox.ToolTip = "Enter new name";
+            emailBox.ToolTip = "Enter new email";
+
+            if (nameBox.Text != "" && emailBox.Text != "")
+            {                
+                EnterNewUserData();
+            }            
+        }
+
+        private void EnterNewUserData()
+        {            
+            foreach (var item in userList)
+            {
+                if (item == userListBox.SelectedItem)
+                {
+                    item.Name = nameBox.Text;                    
+                    item.Email = emailBox.Text;
+                    nameBox.Text = "";
+                    emailBox.Text = "";                    
+                    userListBox.ItemsSource = userList;
+                    userListBox.Items.Refresh();
                 }
             }
         }
