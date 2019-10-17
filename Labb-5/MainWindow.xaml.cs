ae6e86a587899d1c8d85f6560518b44fbec7a2bd
Andreea Nenciu <andreeanen@yahoo.com>
2019-10-17 14:01:09 +0200

    Changed EmailIsUnoque() so that it can be used on Add and Change buttons

--- Labb-5/MainWindow.xaml.cs
+++ Labb-5/MainWindow.xaml.cs
@@ -112,7 +112,7 @@ namespace Labb_5
             }
         }
 
-        private bool EmailIsUnique()
+        private bool EmailIsUnique(string text)
         {
             List<User> allUsers = new List<User>();
             allUsers.AddRange(userList);
@@ -120,9 +120,9 @@ namespace Labb_5
 
             foreach (var user in allUsers)
             {
-                if (user.Email == emailBox.Text)
+                if (user.Email == text)
                 {
-                    MessageBox.Show($"E-Mail address \"{emailBox.Text}\" already registered.\nChoose another E-Mail address.", "E-Mail already in use",
+                    MessageBox.Show($"E-Mail address \"{text}\" already registered.\nChoose another E-Mail address.", "E-Mail already in use",
                                     MessageBoxButton.OK, MessageBoxImage.Exclamation);
                     emailBox.Text = "";
                     emailBox.Focus();
@@ -134,7 +134,7 @@ namespace Labb_5
 
         private void OnAddButtonClicked(object sender, RoutedEventArgs e)
         {
-            if (IsNameValid(nameBox.Text) && IsStringValidEmail(emailBox.Text) && EmailIsUnique())
+            if (IsNameValid(nameBox.Text) && IsStringValidEmail(emailBox.Text) && EmailIsUnique(emailBox.Text))
             {
                 MessageBoxResult answer = MessageBox.Show($"Are you sure you want to add the following user to the User List?\n\n" +
                            $"{"Name:",11}    {nameBox.Text}\n" +
@@ -159,7 +159,7 @@ namespace Labb_5
             EnableDeleteButton();
             EnableMoveUserToAdminButton();
             addUserButton.IsEnabled = true;
-            
+
             foreach (var user in userList)
             {
                 if (user == userListBox.SelectedItem)
@@ -182,7 +182,7 @@ namespace Labb_5
             eMailLabel.Content = "New E-mail";
             nameBox.ToolTip = "Enter new name";
             emailBox.ToolTip = "Enter new email";
-            if (nameBox.Text != "" && emailBox.Text != "" && IsStringValidEmail(emailBox.Text) && IsNameValid(nameBox.Text))
+            if (nameBox.Text != "" && emailBox.Text != "" && IsStringValidEmail(emailBox.Text) && IsNameValid(nameBox.Text) && EmailIsUnique(emailBox.Text))
             {
                 MessageBoxResult answer = MessageBox.Show($"Are you sure you want to change the following user?\n\n" +
                             $"{userInfoLabel.Content}",
